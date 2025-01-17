7100c9dc0bc9afa37e81b894037b7fe59564bb7f
errr-r <luj13014@gmail.com>
2025-01-16 21:17:27 -0500

    Create illuminated_worlds.js

---
+++ candela_obscura/illuminated_worlds.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         光明世界D6池
+// @author       裗浳(3612464276)
+// @version      1.0.0
+// @description  
+// @homepageURL  https://github.com/errrr-er/alll/tree/main
+// @updateUrl    https://raw.githubusercontent.com/errrr-er/alll/refs/heads/main/
+// @updateUrl    https://ghp.ci/https://raw.githubusercontent.com/errrr-er/alll/refs/heads/main/
+// ==/UserScript==
+
