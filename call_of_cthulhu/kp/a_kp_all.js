d362c21ff8259f3dbf11aec8461daa501a532c8f
errr-r <luj13014@gmail.com>
2025-09-05 17:21:08 -0400

    修

--- call_of_cthulhu/kp/a_kp_all.js
+++ call_of_cthulhu/kp/a_kp_all.js
@@ -3,7 +3,7 @@
 // @author       3987681449
 // @version      2.1.0
 // @description  有问题可进群2150284119联系
-// @timestamp    1756784045
+// @timestamp    1757107227
 // 2025-05-11 16:49:17
 // @license      Apache-2
 // @homepageURL  https://github.com/errrr-er/alll/tree/main
