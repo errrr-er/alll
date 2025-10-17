85b93c1a66f2cd16fd7869bfb8664ca2b51cc89f
errrr-er <luj13014@gmail.com>
2025-10-17 14:45:40 -0400

    Update a_kp_all.js

--- call_of_cthulhu/kp/a_kp_all.js
+++ call_of_cthulhu/kp/a_kp_all.js
@@ -68,7 +68,7 @@ async function checkUpdateOnce(ctx, msg, userId) {
         if (githubVersion.timestamp > currentTimestamp) {
             setTimeout(() => {
                 seal.replyToSender(ctx, msg, 
-                    `发现新版本！\n最后更新：{githubVersion.formattedDate}\n更新检查冷却开始`
+                    `发现新版本！\n最后更新：${githubVersion.formattedDate}\n更新检查冷却开始`
                 );
             }, 1000);
         }
