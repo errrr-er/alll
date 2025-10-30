10eedfbe1ca7b05e17df364a0e789328c64c608b
errrr-er <luj13014@gmail.com>
2025-10-30 08:50:37 -0400

    Update a_kp_all.js

--- call_of_cthulhu/kp/a_kp_all.js
+++ call_of_cthulhu/kp/a_kp_all.js
@@ -1,9 +1,9 @@
 // ==UserScript==
 // @name         KP群汇总
 // @author       3987681449
-// @version      4.1.0
+// @version      4.1.1
 // @description  (.kp)有问题可进群2150284119联系
-// @timestamp    1761333891
+// @timestamp    1761828614
 // 2025-05-11 16:49:17
 // @license      Apache-2
 // @homepageURL  https://github.com/errrr-er/alll/tree/main
@@ -13,13 +13,13 @@
 
 let ext = seal.ext.find('KP群汇总');
 if (!ext) {
-  ext = seal.ext.new('KP群汇总', 'er', '4.1.0');
+  ext = seal.ext.new('KP群汇总', 'er', '4.1.1');
   seal.ext.register(ext);
 }
 
 // 时间戳(需要手动更新)
 function getCurrentTimestamp() {
-    return 1761333891;
+    return 1761828614;
 }
 
 // 提醒历史
