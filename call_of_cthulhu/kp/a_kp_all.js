310dfe6f764c8efd5cba1378c656dbe5073327c2
errrr-er <luj13014@gmail.com>
2025-11-03 12:33:48 -0500

    Update a_kp_all.js

--- call_of_cthulhu/kp/a_kp_all.js
+++ call_of_cthulhu/kp/a_kp_all.js
@@ -3,7 +3,7 @@
 // @author       3987681449
 // @version      4.2.0
 // @description  (.kp)有问题可进群2150284119联系
-// @timestamp    1762185374
+// @timestamp    1762185375
 // 2025-05-11 16:49:17
 // @license      Apache-2
 // @homepageURL  https://github.com/errrr-er/alll/tree/main
@@ -19,7 +19,7 @@ if (!ext) {
 
 // 时间戳(需要手动更新)
 function getCurrentTimestamp() {
-    return 1762185374;
+    return 1762185375;
 }
 
 // 提醒历史
